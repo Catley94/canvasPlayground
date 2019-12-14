3c3dc8696b56da582aa8db9b0cf93bce1cbb3321
Samuel Catley <scp94373@gmail.com>
2019-12-14 21:17:20 +0000

    enlarging circles

--- canvas.js
+++ canvas.js
@@ -36,17 +36,116 @@ var c = canvas.getContext('2d');
 // c.stroke();
 // }
 
+// var x = Math.random() * innerWidth;
+// var y = Math.random() * innerHeight;
+// var dx = (Math.random() - 0.5) * 8;
+// var dy = (Math.random() - 0.5) * 8;
+// var radius = 30;
+
 //Arc / Circle
 
-var x = 200;
-function animate() {
-    requestAnimationFrame(animate);
-    c.clearRect(0, 0,innerWidth, innerHeight);
-    c.beginPath();
-    c.arc(x, 200, 30, 0, Math.PI * 2, false);
-    c.strokeStyle = 'blue';
-    c.stroke();
-
-    x+= 1;
+var mouse = {
+    x: undefined,
+    y: undefined
 }
 
+// var minRadius = 2;
+var maxRadius = 40;
+
+var colorArray = [
+    '#1E4040',
+    '#E65B3E',
+    '#E7DFCA',
+    '#E6E6E6',
+    '#F7F7F7'
+];
+
+window.addEventListener('mousemove', function(event) {
+    mouse.x = event.x;
+    mouse.y = event.y;
+})
+
+window.addEventListener('resize', function() {
+    canvas.width = window.innerWidth;
+    canvas.height = window.innerHeight;
+
+    init();
+})
+
+function Circle(x, y, dx, dy, radius) {
+    this.x = x;
+    this.y = y;
+    this.dx = dx;
+    this.dy = dy;
+    this.radius = radius;
+    this.minRadius = radius;
+    this.color = colorArray[Math.floor(Math.random() * colorArray.length)];
+
+    this.draw = function() {
+        c.beginPath();
+        c.arc(this.x, this.y, this.radius, 0, Math.PI * 2, false);
+        c.fillStyle = this.color;
+        c.fill();
+    }
+
+    this.update = function() {
+        if(this.x + this.radius > innerWidth || this.x - this.radius < 0) {
+            this.dx = -this.dx;
+        }
+        if(this.y + this.radius > innerHeight || this.y - this.radius < 0 ) {
+            this.dy = -this.dy;
+        }
+        this.x += this.dx;
+        this.y += this.dy;
+
+        //Interactivity
+
+        if(mouse.x - this.x < 50 && mouse.x - this.x > -50 && mouse.y - this.y < 50 && mouse.y - this.y > -50) {
+            if(this.radius < maxRadius) {
+                this.radius += 1;
+            }
+            
+        } else if (this.radius > this.minRadius){
+            this.radius -= 1;
+        }
+
+
+
+        this.draw();
+    }
+}
+
+
+
+var circleArray = [];
+
+
+// circle.update();
+
+
+
+function init() {
+
+    circleArray = [];
+    for (var i = 0; i < 500; i++) {
+        var radius = Math.random() * 3 + 1;
+        var x = Math.random() * (innerWidth - radius * 2) + radius;
+        var y = Math.random() * (innerHeight - radius * 2) + radius;
+        var dx = (Math.random() - 0.5);
+        var dy = (Math.random() - 0.5);
+        circleArray.push(new Circle(x, y, dx, dy, radius));
+    }
+}
+
+function animate() {
+    requestAnimationFrame(animate);
+    c.clearRect(0, 0,innerWidth, innerHeight);   
+
+    for(var i = 0; i < circleArray.length; i++) {
+        circleArray[i].update();
+    }
+}
+
+
+animate();
+init();
